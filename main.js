d3be5e71a45f9cf1eab659907015f84d28a1b04b
monalika-m <77562020+monalika-m@users.noreply.github.com>
2021-08-07 23:11:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function seend() {
+    donut = document.getElementById("name_of_the_student_1").value
+   salad = document.getElementById("name_of_the_student_2").value
+   tesla_rari = document.getElementById("name_of_the_student_3").value 
+    aeidem = document.getElementById("name_of_the_student_4").value
+    if(donut=="donut"){
+        document.getElementById("result1").innerHTML=✔
+    }
+    else{
+        document.getElementById("result1").innerHTML=✗
+    }
+    if(salad=="salad"){
+        document.getElementById("resultreee").innerHTML=✔
+    }
+    else{
+        document.getElementById("resultreee").innerHTML=✗
+    }
+    if(tesla_rari=="tesla rari"){
+        document.getElementById("result3").innerHTML=✔
+    }
+    else{
+        document.getElementById("result3").innerHTML=✗
+    }
+    if(aeidem=="aeidem"){
+        document.getElementById("result4").innerHTML=✔
+    }
+    else{
+        document.getElementById("result4").innerHTML=✗
+    }
+}
